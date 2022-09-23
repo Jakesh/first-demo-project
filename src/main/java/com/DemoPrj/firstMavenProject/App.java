d5bcf8fa0d3c0b0ae476c598efed8affc4ae8e28
Jakesh <67775273+Jakesh@users.noreply.github.com>
2022-09-23 20:19:20 +0530

    first commit

---
+++ src/main/java/com/DemoPrj/firstMavenProject/App.java
@@ -0,0 +1,13 @@
+package com.DemoPrj.firstMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
